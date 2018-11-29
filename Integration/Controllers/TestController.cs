20f468b7e310ffc85cec8f151256fbed821cb3af
Shreerathna S <shreerathna.s@hp.com>
2018-11-29 12:28:10 +0530

    Comitted test controller

---
+++ Integration/Controllers/TestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Integration.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string  Index()
+        {
+            return "Test Controller";
+        }
+    }
+}
